eb12deff03ca51edfdae7e5350a2f3e93ae8fa6c
Amaury Gomez <amaurygomez1993@gmail.com>
2012-10-09 15:20:22 -0400

    More changes xD

---
+++ src/modelos/Recursos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Amaury Gomez
+ */
+public class Recursos {
+ 
+
+
+public static void  entrar(){
+   ArrayList<Profesor> Profesores ;
+ArrayList<Materia> Materias ;
+
+
+}
+}
